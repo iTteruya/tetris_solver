287517d83f65e32c23e6e7bf1707dc07c0a43b2d
itTeruya <timur.ivanov@outlook.com>
2020-12-09 14:51:41 +0300

    solver realised

--- core/src/com/mygdx/tetslv/model/GameField.java
+++ core/src/com/mygdx/tetslv/model/GameField.java
@@ -4,120 +4,141 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.Arrays;
 
 public class GameField extends Actor {
 
     public static final int CELL_SIZE = 32;
+    public static final int NUM_COLUMNS = 20;
+    public static final int NUM_ROWS = 30;
 
-    static final int INDEX_COLUMN = 0;
-    static final int INDEX_ROW = 1;
+    private int[][] matrix;
+    private int rows;
+    private int columns;
+    private int score;
 
-    public static final int NUM_COLUMNS = 10;
-    public static final int NUM_ROWS = 20;
-    private boolean solverStatus;
-
-    private boolean[][] isFilled = new boolean[NUM_COLUMNS][NUM_ROWS];
-    private ShapeRenderer shapeRenderer = new ShapeRenderer();
-
-    public void reset() {
-        for (int i = 0; i < NUM_COLUMNS; i++) {
-            for (int j = 0; j < NUM_ROWS; j++) {
-                isFilled[i][j] = false;
-            }
+    public GameField() {
+        rows = NUM_ROWS;
+        columns = NUM_COLUMNS;
+        matrix = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            Arrays.fill(matrix[i], 0);
         }
     }
 
-    private boolean isLineFilled(int l) {
-        for (int c = 0;  c < NUM_COLUMNS; c++) {
-            if (!isFilled[c][l]) {
-                return false;
+    public GameField(GameField gameField) {
+        rows = NUM_ROWS;
+        columns = NUM_COLUMNS;
+        score = gameField.getScore();
+        matrix = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            if (columns >= 0) System.arraycopy(gameField.matrix[i], 0, matrix[i], 0, columns);
+        }
+    }
+
+    public void reset(){
+        rows = NUM_ROWS;
+        columns = NUM_COLUMNS;
+        score = 0;
+        matrix = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            Arrays.fill(matrix[i], 0);
+        }
+    }
+
+    private boolean addFigure(Piece piece) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int fi = piece.getCurrentRow() + i;
+                int fj = piece.getCurrentColumn() + j;
+                int l = rows;
+                int value = piece.matrix[i][j];
+                if (value == 1) {
+                    if (fi < l && fi >= 0 && fj < l && fj >= 0) {
+                        matrix[fi][fj] = 1;
+                    }
+                }
             }
         }
         return true;
     }
 
-    public int placePiece(int[][] cells) {
-        for (int[] cell: cells) {
-            isFilled[cell[INDEX_COLUMN]][cell[INDEX_ROW]] = true;
-        }
-
-        Set<Integer> linesToRemove = new HashSet<>();
-        for (int r = 0; r < NUM_ROWS; r++) {
-            if (isLineFilled(r)) {
-                linesToRemove.add(r);
-            }
-        }
-
-        if (linesToRemove.isEmpty()) {
-            return 0;
-        }
-
-        int delta = 0;
-        int newLine = -1;
-        for (int r = 0; r < NUM_ROWS; r++) {
-            if (isLineFilled(r) && r > newLine) {
-                delta++;
-                newLine = r + 1;
-                if (r < NUM_ROWS - 2) {
-                    while (isLineFilled(newLine) && newLine != NUM_ROWS) {
-                        delta++;
-                        newLine++;
+    public boolean update(Piece piece) {
+        int[][] pieceMatrix = piece.matrix;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int row = piece.getCurrentRow();
+                int column = piece.getCurrentColumn();
+                if (pieceMatrix[i][j] == 1) {
+                    if (row + i == 0) {
+                        return addFigure(piece);
+                    } else if ((row + i - 1 < rows && column + j < columns)) {
+                        if ((matrix[row + i - 1][column + j]) == 1) {
+                            return addFigure(piece);
+                        }
                     }
                 }
-            }
-            if (r < NUM_ROWS - 2) {
-                if (!isLineFilled(r) && isLineFilled(r + 1)) {
-                    delta = 0;
-                    newLine = r;
-                }
-            }
-
-            if (r + delta < NUM_ROWS) {
-                for (int c = 0; c < NUM_COLUMNS; c++) {
-                    isFilled[c][r] = isFilled[c][r + delta];
-                }
-            } else {
-                for (int c = 0; c < NUM_COLUMNS; c++) {
-                    isFilled[c][r] = false;
-                }
-            }
-        }
-        return linesToRemove.size();
-    }
-
-    public boolean isOnGround(int[][] cells) {
-        for (int[] cell: cells) {
-            if (cell[INDEX_ROW] - 1 >= NUM_ROWS) {
-                continue;
-            }
-            if (cell[INDEX_ROW] <= 0 || isFilled[cell[INDEX_COLUMN]][cell[INDEX_ROW] - 1]) {
-                return true;
             }
         }
         return false;
     }
 
-    boolean canPlace(int[][] cells) {
-        for (int[] cell: cells) {
-            int row = cell[INDEX_ROW];
-            int column = cell[INDEX_COLUMN];
-            if (row < 0 || column < 0 || row >= NUM_ROWS || column >= NUM_COLUMNS || isFilled[column][row]) {
-                return false;
+    public void clearLine(boolean solverON) {
+        boolean lineIsFilled = false;
+        for (int i = 0; i < rows; i++) {
+            int counter = 0;
+            for (int j = 0; j < columns; j++) {
+                if (lineIsFilled && i + 1 < rows) {
+                    matrix[i - 1][j] = matrix[i][j];
+                } else if (matrix[i][j] == 1) {
+                    counter++;
+                } else if (counter != 0) {
+                    break;
+                }
             }
+            if (counter >= NUM_COLUMNS && !lineIsFilled) {
+                lineIsFilled = true;
+                if (solverON) score--;
+                else score++;
+                for (int j = 0; j < columns; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public boolean gameIsNotOver() {
+        for (int i = 0; i < columns; i++) {
+            if(matrix[rows-1][i] == 1) return false;
         }
         return true;
     }
 
-    public GameField(boolean solverStatus) {
-        this.solverStatus = solverStatus;
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
+
+    int[][] getMatrix() {
+        return matrix;
+    }
+
+    int getRows() {
+        return rows;
+    }
+
+    int getCols() {
+        return columns;
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         batch.end();
+        ShapeRenderer shapeRenderer = new ShapeRenderer();
+
         shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
         shapeRenderer.setTransformMatrix(batch.getTransformMatrix());
         shapeRenderer.translate(getX(), getY(), 0);
@@ -135,35 +156,14 @@ public class GameField extends Actor {
         shapeRenderer.setColor(Color.BLACK);
         for (int i = 0; i * 32 < height; i++ ) {
             for (int j = 0; j * 32 < width; j++) {
-                shapeRenderer.rect(1, 1, 32 + 32 * j, 32 + 32 * i);
+                shapeRenderer.rect(1, 1, CELL_SIZE + CELL_SIZE * j,  CELL_SIZE + CELL_SIZE * i);
             }
         }
+
         shapeRenderer.end();
-
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        if (solverStatus) shapeRenderer.setColor(Color.SCARLET);
-        else shapeRenderer.setColor(Color.ROYAL);
-        for (int i = 0; i < NUM_COLUMNS; i++) {
-            for (int j = 0; j < NUM_ROWS; j++) {
-                if (isFilled[i][j]) {
-                    shapeRenderer.rect(i * CELL_SIZE + 1, j * CELL_SIZE + 1, CELL_SIZE, CELL_SIZE);
-                }
-            }
-        }
-        shapeRenderer.end();
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.setColor(Color.BLACK);
-        for (int i = 0; i < NUM_COLUMNS; i++) {
-            for (int j = 0; j < NUM_ROWS; j++) {
-                if (isFilled[i][j]) {
-                    shapeRenderer.rect(i * CELL_SIZE + 1, j * CELL_SIZE + 1, CELL_SIZE, CELL_SIZE);
-                }
-            }
-        }
-        shapeRenderer.end();
-
         batch.begin();
+
     }
+
 }
+
